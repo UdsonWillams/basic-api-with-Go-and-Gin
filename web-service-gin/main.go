08df9f3733f7f3f62d013dd4114748f3c88f2442
UdsonWillams <udson.willams@gmail.com>
2024-05-13 18:00:15 -0300

    add news endpoints

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -6,71 +6,49 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type WordsBody struct {
+	Words []string `json:"words"`
+	Order string   `json:"order"`
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+type VowelsBody struct {
+	Vowels []string `json:"vowels"`
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+type WordsOrder struct {
+	asc  string
+	desc string
 }
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+func postOrderWords(c *gin.Context) {
+	var orderWords WordsBody
+	if err := c.BindJSON(&orderWords); err != nil {
 		return
 	}
+	response := SortWords(orderWords.Words, orderWords.Order)
+	c.IndentedJSON(http.StatusOK, response)
+}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+func postCountVowels(c *gin.Context) {
+	var countVowels VowelsBody
+	if err := c.BindJSON(&countVowels); err != nil {
+		return
+	}
+	response := CountVowels(countVowels.Vowels)
+	c.IndentedJSON(http.StatusOK, response)
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
-	// In Gin, the colon preceding an item in the path signifies that the item is a path parameter.
+	// In Gin, the colon preceding an item in the path
+	// signifies that the item is a path parameter.
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
-	router.Run("localhost:8080")
+	// My own examples.
+	router.POST("/words/order", postOrderWords)
+	router.POST("/words/count-vowels", postCountVowels)
+
+	router.Run("localhost:8081")
 }
-
-// Congratulations! You’ve just used Go and Gin to write a simple RESTful web service.
-
-// Suggested next topics:
-
-//     If you’re new to Go, you’ll find useful best practices described in Effective Go and How to write Go code.
-//     The Go Tour is a great step-by-step introduction to Go fundamentals.
-//     For more about Gin, see the Gin Web Framework package documentation or the Gin Web Framework docs.
